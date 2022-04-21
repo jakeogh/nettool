aebdbdcb5c94fed9ad903b2a8149af1459a4c2eb
jakeogh <github.com@v6y.net>
2022-04-21 15:09:38 -0700

    auto-commit

--- nettool/nettool.py
+++ nettool/nettool.py
@@ -46,11 +46,11 @@ from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
 from eprint import eprint
+from mptool import unmp
 from pathtool import read_file_bytes
 from retry_on_exception import retry_on_exception
-from unmp import unmp
 
-signal(SIGPIPE,SIG_DFL)
+signal(SIGPIPE, SIG_DFL)
 
 
 def get_timestamp():
@@ -61,13 +61,14 @@ def get_timestamp():
 try:
     from scapy.all import get_windows_if_list
 except ImportError:
+
     def get_windows_if_list():
         assert False
 
 
 def get_network_interfaces():
     ports = netifaces.interfaces()
-    skip_interfaces = ['lo', 'dummy0', 'teql0']
+    skip_interfaces = ["lo", "dummy0", "teql0"]
     for interface in skip_interfaces:
         try:
             ports.remove(interface)
@@ -77,20 +78,21 @@ def get_network_interfaces():
 
 
 def get_name_for_windows_network_uuid(uuid):
-    if not uuid.startswith('{'):
-        return uuid     # return non win device, should tuple
+    if not uuid.startswith("{"):
+        return uuid  # return non win device, should tuple
 
-    assert uuid.endswith('}')
+    assert uuid.endswith("}")
     for item in get_windows_if_list():
-        if item['guid'] == uuid:
-            return (item['name'], item['description'])
+        if item["guid"] == uuid:
+            return (item["name"], item["description"])
     raise ValueError(uuid)
 
 
-def get_ip_addresses_for_interface(*,
-                                   interface: str,
-                                   verbose: Union[bool, int, float],
-                                   ):
+def get_ip_addresses_for_interface(
+    *,
+    interface: str,
+    verbose: Union[bool, int, float],
+):
     addresses = netifaces.ifaddresses(interface)
     if verbose == inf:
         ic(addresses)
@@ -98,69 +100,72 @@ def get_ip_addresses_for_interface(*,
         addresses = addresses[netifaces.AF_INET]
     except KeyError:
         return []
-    addresses = [ip['addr'] for ip in addresses]
+    addresses = [ip["addr"] for ip in addresses]
     if verbose == inf:
         ic(addresses)
     return addresses
 
 
-def get_mac_for_interface(interface: str,
-                          verbose: Union[bool, int, float],
-                          ):
-    mac = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+def get_mac_for_interface(
+    interface: str,
+    verbose: Union[bool, int, float],
+):
+    mac = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]["addr"]
     if verbose:
         ic(mac)
-    mac = ''.join(mac.split(':'))
+    mac = "".join(mac.split(":"))
     mac = bytes.fromhex(mac)
     if verbose:
         ic(mac)
     return mac
 
 
-def construct_proxy_dict(verbose: Union[bool, int, float],
-                         ):
-    proxy_config = read_file_bytes('/etc/portage/proxy.conf').decode('utf8').split('\n')
+def construct_proxy_dict(
+    verbose: Union[bool, int, float],
+):
+    proxy_config = read_file_bytes("/etc/portage/proxy.conf").decode("utf8").split("\n")
     if verbose:
         ic(proxy_config)
     proxy_dict = {}
     for line in proxy_config:
         if verbose == inf:
             ic(line)
-        scheme = line.split('=')[0].split('_')[0]
-        line = line.split('=')[-1]
+        scheme = line.split("=")[0].split("_")[0]
+        line = line.split("=")[-1]
         line = line.strip('"')
-        #scheme = line.split('://')[0]
+        # scheme = line.split('://')[0]
         if verbose == inf:
             ic(scheme)
         proxy_dict[scheme] = line
-        #proxy = line.split('://')[-1].split('"')[0]
+        # proxy = line.split('://')[-1].split('"')[0]
     ic(proxy_dict)
     return proxy_dict
 
 
 # todo, add asc_sig options
 @retry_on_exception(exception=ConnectionError)
-def download_file(*,
-                  url: str,
-                  destination_dir: Optional[Path] = None,
-                  force: bool = False,
-                  proxy_dict: Optional[dict] = None,
-                  progress: bool = False,
-                  verbose: Union[bool, int, float],
-                  ):
+def download_file(
+    *,
+    url: str,
+    destination_dir: Optional[Path] = None,
+    force: bool = False,
+    proxy_dict: Optional[dict] = None,
+    progress: bool = False,
+    verbose: Union[bool, int, float],
+):
 
     eprint("downloading:", url)
     if destination_dir:
         destination_dir = Path(destination_dir)
-        local_filename = destination_dir /  Path(url.split('/')[-1])
+        local_filename = destination_dir / Path(url.split("/")[-1])
     else:
         local_filename = None
 
-    #if force:
+    # if force:
     #    os.unlink(local_filename)
 
-    #proxy_dict = {}
-    #if proxy:
+    # proxy_dict = {}
+    # if proxy:
     #    verify(not proxy.startswith('http'))
     #    verify(len(proxy.split(":")) == 2)
     #    proxy_dict["http"] = proxy
@@ -171,12 +176,12 @@ def download_file(*,
     byte_count = 0
     if local_filename:
         try:
-            with open(local_filename, 'bx') as fh:
-                for chunk in r.iter_content(chunk_size=1024*1024):
+            with open(local_filename, "bx") as fh:
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
                     if chunk:
                         fh.write(chunk)
                     if progress:
-                        eprint('bytes:', byte_count)
+                        eprint("bytes:", byte_count)
         except FileExistsError:
             eprint("skipping download, file exists:", local_filename.as_posix())
         r.close()
@@ -187,36 +192,49 @@ def download_file(*,
     return text
 
 
-
 @click.command()
 @click.argument("interfaces", type=str, nargs=-1)
 @click_add_options(click_global_options)
 @click.pass_context
-def cli(ctx,
-        interfaces: Optional[Tuple[str, ...]],
-        verbose: Union[bool, int, float],
-        verbose_inf: bool,
-        ):
+def cli(
+    ctx,
+    interfaces: Optional[Tuple[str, ...]],
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+    dict_input: bool,
+):
 
     ctx.ensure_object(dict)
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
     if interfaces:
         iterator = interfaces
     else:
-        iterator = unmp(valid_types=[str,], verbose=verbose,)
+        iterator = unmp(
+            valid_types=[
+                str,
+            ],
+            verbose=verbose,
+        )
 
     index = 0
     for index, interface in enumerate(iterator):
         if verbose:
             ic(index, interface)
 
-        print(get_ip_addresses_for_interface(interface=interface,
-                                             verbose=verbose,
-                                             ))
-        print(get_mac_for_interface(interface=interface,
-                                    verbose=verbose,
-                                    ))
+        print(
+            get_ip_addresses_for_interface(
+                interface=interface,
+                verbose=verbose,
+            )
+        )
+        print(
+            get_mac_for_interface(
+                interface=interface,
+                verbose=verbose,
+            )
+        )
