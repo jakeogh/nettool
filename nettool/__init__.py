e4b00fd358cb241c6dcabb9368fd11e96dac32e5
jakeogh <github.com@v6y.net>
2022-10-28 17:59:15 -0700

    auto-commit

--- nettool/__init__.py
+++ nettool/__init__.py
@@ -2,4 +2,5 @@ from .nettool import construct_proxy_dict
 from .nettool import download_file
 from .nettool import get_ip_addresses_for_interface
 from .nettool import get_mac_for_interface
+from .nettool import internet_available
 from .nettool import tcp_port_in_use
