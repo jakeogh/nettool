6235020ffb092ecd0fda4a4ab49e4f85caa73ae9
jakeogh <github.com@v6y.net>
2024-11-04 18:51:03 -0700

    auto-commit

--- nettool/__init__.py
+++ nettool/__init__.py
@@ -1,10 +1,15 @@
-from .nettool import AliasExistsError
-from .nettool import add_alias
-from .nettool import delete_alias
-from .nettool import download_file
-from .nettool import get_default_gateway
-from .nettool import get_hostname
-from .nettool import get_ip_addresses_for_interface
-from .nettool import get_mac_for_interface
-from .nettool import internet_available
-from .nettool import tcp_port_in_use
+# -*- coding: utf-8 -*-
+"""
+isort:skip_file
+"""
+
+from .nettool import AliasExistsError as AliasExistsError
+from .nettool import add_alias as add_alias
+from .nettool import delete_alias as delete_alias
+from .nettool import download_file as download_file
+from .nettool import get_default_gateway as get_default_gateway
+from .nettool import get_hostname as get_hostname
+from .nettool import get_ip_addresses_for_interface as get_ip_addresses_for_interface
+from .nettool import get_mac_for_interface as get_mac_for_interface
+from .nettool import internet_available as internet_available
+from .nettool import tcp_port_in_use as tcp_port_in_use
