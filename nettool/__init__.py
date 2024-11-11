b05d2ef3a18f03a20425451a649e1fd54d4a6f88
jakeogh <github.com@v6y.net>
2024-11-11 01:49:03 -0700

    auto-commit

--- nettool/__init__.py
+++ nettool/__init__.py
@@ -14,3 +14,4 @@ from .nettool import get_mac_for_interface as get_mac_for_interface
 from .nettool import internet_available as internet_available
 from .nettool import tcp_port_in_use as tcp_port_in_use
 from .nettool import get_network_ports as get_network_ports
+from .nettool import generate_network_port_help as generate_network_port_help
