116ba04a8243bb0e997ce3e7ff3fcc96c4729c30
jakeogh <github.com@v6y.net>
2025-10-19 02:41:31 -0700

    auto-commit

--- nettool/nettool.py
+++ nettool/nettool.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=fixme                           # [W0511] todo is encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 
 from __future__ import annotations
 
@@ -33,9 +16,7 @@ import netifaces
 import requests
 import sh
 from asserttool import ic
-from asserttool import icp
 from eprint import eprint
-from globalverbose import gvd
 from retry_on_exception import retry_on_exception
 
 signal(SIGPIPE, SIG_DFL)
@@ -67,11 +48,21 @@ def get_network_interfaces() -> list[str]:
 
 
 def set_interface_link_up(interface: str) -> None:
-    sh.ip("link", "set", "up", interface)
+    sh.ip(
+        "link",
+        "set",
+        "up",
+        interface,
+    )
 
 
 def set_interface_link_down(interface: str) -> None:
-    sh.ip("link", "set", "down", interface)
+    sh.ip(
+        "link",
+        "set",
+        "down",
+        interface,
+    )
 
 
 def interface_link_is_up(interface: str) -> bool:
@@ -100,7 +91,13 @@ def alias_add(*, ip_with_subnet: str, device: str):
     ip_command = sh.Command("ip")
     result = None
     try:
-        result = ip_command("address", "add", ip_with_subnet, "dev", device)
+        result = ip_command(
+            "address",
+            "add",
+            ip_with_subnet,
+            "dev",
+            device,
+        )
     except sh.ErrorReturnCode_2 as e:
         ic(e)
         ic(e.args)
@@ -116,7 +113,13 @@ def alias_add(*, ip_with_subnet: str, device: str):
 
 def alias_remove(*, ip_with_subnet: str, device: str):
     assert "/" in ip_with_subnet
-    sh.ip("address", "del", ip_with_subnet, "dev", device)
+    sh.ip(
+        "address",
+        "del",
+        ip_with_subnet,
+        "dev",
+        device,
+    )
 
 
 # https://public-dns.info/nameservers.txt
@@ -253,7 +256,12 @@ def download_file(
     #    proxy_dict["https"] = proxy
 
     ic(proxy_dict)
-    r = requests.get(url, stream=True, proxies=proxy_dict, timeout=60)
+    r = requests.get(
+        url,
+        stream=True,
+        proxies=proxy_dict,
+        timeout=60,
+    )
     byte_count = 0
     if local_filename:
         try:
@@ -272,36 +280,6 @@ def download_file(
     r.close()
     return text
 
-
-# def get_udp_packet(
-#    *,
-#    client_ip_address: str,
-#    destination_port: str,
-# ):
-#    ic(client_ip_address, destination_port)
-#
-#    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-#    sock.bind((client_ip_address, destination_port))
-#
-#    # while True:
-#    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-#    print("received message: %s" % data)
-#    return data
-#
-#
-# def get_ip_addresses_for_interface(
-#    interface: str,
-# ):
-#    addresses = netifaces.ifaddresses(interface)
-#    if gvd:
-#        ic(addresses)
-#    try:
-#        addresses = addresses[netifaces.AF_INET]
-#    except KeyError:
-#        return []
-#    addresses = [ip["addr"] for ip in addresses]
-#    ic(addresses)
-#    return addresses
 
 # def get_mac_for_interface(
 #    interface: str,
